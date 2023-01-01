5f3d27a6c9a49e7f3cd2fd0f8ba50f52ccec17ad
luckyLacchin <gabrielelacchin92@gmail.com>
2023-01-01 19:24:20 +0100

    test: aggiunta in Calendario POST

--- __tests__/calendar/Calendario-POST.test.js
+++ __tests__/calendar/Calendario-POST.test.js
@@ -37,6 +37,12 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
       email: "utenteTestCalendarioPOSTDuplicato@unitn.it",
       username: "utenteTestCalendarioPOSTDuplicato",
     });
+
+    await UtenteAutenticato.insertOne({
+      userId: "utenteTestCalendarioPOSTPartecipante",
+      email: "utenteTestCalendarioPOSTPartecipante@unitn.it",
+      username: "utenteTestCalendarioPOSTPartecipante",
+    });
   });
 
   afterAll(async () => {
@@ -141,7 +147,7 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
       );
     });
 
-    test("Calendario inserito con successo con userId, nome, fusoOrario, principale", async () => {
+    test("Calendario inserito con successo con userId, nome, fusoOrario, colore, principale, partecipanti", async () => {
       const { req, res } = createMocks({
         method: "POST",
         query: {
@@ -151,14 +157,53 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
             GMTOffset: -5,
             localita: "New York",
           },
+          colore: "#00FF00",
           principale: false,
+          partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
         },
       });
 
       await creaCalendario(req, res);
 
       expect(res._getStatusCode()).toBe(200);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          success: "Calendar inserted correctly",
+        })
+      );
+    });
 
+    test("Calendario inserito con successo con userId, nome, fusoOrario, colore, principale, partecipanti, impostazioniPredefiniteEventi", async () => {
+      const { req, res } = createMocks({
+        method: "POST",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "calendarioTestProva1",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#00FF00",
+          principale: false,
+          partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(200);
       expect(JSON.parse(res._getData())).toEqual(
         expect.objectContaining({
           success: "Calendar inserted correctly",
@@ -179,7 +224,6 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
       await creaCalendario(req, res);
 
       expect(res._getStatusCode()).toBe(200);
-
       expect(JSON.parse(res._getData())).toEqual(
         expect.objectContaining({
           success: "Calendar inserted correctly",
@@ -194,19 +238,71 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
           userId: "utenteTestCalendarioPOST",
           nome: "calendarioTestProva1",
           colore: "#00FF00",
-          principale: false,
+          principale: false
         },
       });
 
       await creaCalendario(req, res);
 
       expect(res._getStatusCode()).toBe(200);
-
       expect(JSON.parse(res._getData())).toEqual(
         expect.objectContaining({
           success: "Calendar inserted correctly",
         })
       );
+    });
+
+    test("Calendario inserito con successo con userId, nome, colore, principale, partecipanti", async () => {
+      const { req, res } = createMocks({
+        method: "POST",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "calendarioTestProva1",
+          colore: "#00FF00",
+          principale: false,
+          partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+        },  
+      });
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(200);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          success: "Calendar inserted correctly",
+        })
+      );
+    });
+      test("Calendario inserito con successo con userId, nome, colore, principale, partecipanti, impostazioniPredefiniteEventi", async () => {
+        const { req, res } = createMocks({
+          method: "POST",
+          query: {
+            userId: "utenteTestCalendarioPOST",
+            nome: "calendarioTestProva1",
+            colore: "#00FF00",
+            principale: false,
+            partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+            impostazioniPredefiniteEventi: {
+              titolo: "",
+              descrizione: "",
+              durata: 30,
+              tempAnticNotifica: 30,
+              luogo: {
+                latitudine: 12.123456,
+                longitudine: 12.123456,
+              },
+              priorita: 6,
+              difficolta: 6,
+            },
+          },
+        });
+        await creaCalendario(req, res);
+
+        expect(res._getStatusCode()).toBe(200);
+        expect(JSON.parse(res._getData())).toEqual(
+          expect.objectContaining({
+            success: "Calendar inserted correctly",
+          })
+        );
     });
 
     test("Calendario inserito con successo con userId, nome, principale", async () => {
@@ -218,17 +314,163 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
           principale: false,
         },
       });
-
       await creaCalendario(req, res);
 
       expect(res._getStatusCode()).toBe(200);
-
       expect(JSON.parse(res._getData())).toEqual(
         expect.objectContaining({
           success: "Calendar inserted correctly",
         })
       );
     });
+
+      test("Calendario inserito con successo con userId, nome, principale, partecipanti", async () => {
+        const { req, res } = createMocks({
+          method: "POST",
+          query: {
+            userId: "utenteTestCalendarioPOST",
+            nome: "calendarioTestProva1",
+            principale: false,
+            partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+          },
+        });
+        await creaCalendario(req, res);
+
+        expect(res._getStatusCode()).toBe(200);
+        expect(JSON.parse(res._getData())).toEqual(
+          expect.objectContaining({
+            success: "Calendar inserted correctly",
+          })
+        );
+      });
+
+        test("Calendario inserito con successo con userId, nome, principale, partecipanti, impostazioniPredefiniteEventi", async () => {
+          const { req, res } = createMocks({
+            method: "POST",
+            query: {
+              userId: "utenteTestCalendarioPOST",
+              nome: "calendarioTestProva1",
+              principale: false,
+              partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+              impostazioniPredefiniteEventi: {
+                titolo: "",
+                descrizione: "",
+                durata: 30,
+                tempAnticNotifica: 30,
+                luogo: {
+                  latitudine: 12.123456,
+                  longitudine: 12.123456,
+                },
+                priorita: 6,
+                difficolta: 6,
+              },
+            },
+          });
+          await creaCalendario(req, res);
+
+          expect(res._getStatusCode()).toBe(200);
+          expect(JSON.parse(res._getData())).toEqual(
+            expect.objectContaining({
+              success: "Calendar inserted correctly",
+            })
+          );
+        });
+
+        test("Calendario inserito con successo con userId, nome, partecipanti", async () => {
+          const { req, res } = createMocks({
+            method: "POST",
+            query: {
+              userId: "utenteTestCalendarioPOST",
+              nome: "calendarioTestProva1",
+              partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+              impostazioniPredefiniteEventi: {
+                titolo: "",
+                descrizione: "",
+                durata: 30,
+                tempAnticNotifica: 30,
+                luogo: {
+                  latitudine: 12.123456,
+                  longitudine: 12.123456,
+                },
+                priorita: 6,
+                difficolta: 6,
+              },
+            },
+          });
+          await creaCalendario(req, res);
+  
+          expect(res._getStatusCode()).toBe(200);
+          expect(JSON.parse(res._getData())).toEqual(
+            expect.objectContaining({
+              success: "Calendar inserted correctly",
+            })
+          );
+      });
+
+      test("Calendario inserito con successo con userId, nome, partecipanti, impostazioniPredefiniteEventi", async () => {
+        const { req, res } = createMocks({
+          method: "POST",
+          query: {
+            userId: "utenteTestCalendarioPOST",
+            nome: "calendarioTestProva1",
+            partecipanti: ["utenteTestCalendarioPOST", "utenteTestCalendarioPOSTPartecipante"],
+            impostazioniPredefiniteEventi: {
+              titolo: "",
+              descrizione: "",
+              durata: 30,
+              tempAnticNotifica: 30,
+              luogo: {
+                latitudine: 12.123456,
+                longitudine: 12.123456,
+              },
+              priorita: 6,
+              difficolta: 6,
+            },
+          },
+        });
+        await creaCalendario(req, res);
+
+        expect(res._getStatusCode()).toBe(200);
+        expect(JSON.parse(res._getData())).toEqual(
+          expect.objectContaining({
+            success: "Calendar inserted correctly",
+          })
+        );
+    });
+
+    test("Calendario inserito con successo con userId, nome, impostazioniPredefiniteEventi", async () => {
+      const { req, res } = createMocks({
+        method: "POST",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "calendarioTestProva1",
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(200);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          success: "Calendar inserted correctly",
+        })
+      );
+  });
+
+        
+
+
   });
 
   describe("400", () => {
@@ -345,6 +587,229 @@ describe("Test di tutti i casi POST (creazione calendario)", () => {
       expect(JSON.parse(res._getData())).toEqual(
         expect.objectContaining({
           error: "Wrong format for time zone",
+        })
+      );
+    });
+
+    test("Valore impostazioni predefinite eventi non e' valido - latitudine", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 112.123465,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
+        })
+      );
+    });
+    test("Valore impostazioni predefinite eventi non e' valido - longitudine", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 1102.123465,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
+        })
+      );
+    });
+    test("Valore impostazioni predefinite eventi non e' valido - priorita", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 60,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
+        })
+      );
+    });
+    test("Valore impostazioni predefinite eventi non e' valido - difficolta", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: -6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
+        })
+      );
+    });
+    test("Valore impostazioni predefinite eventi non e' valido - durata", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: -30,
+            tempAnticNotifica: 30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
+        })
+      );
+    });
+    test("Valore impostazioni predefinite eventi non e' valido - tempAnticNotifica", async () => {
+      const { req, res } = createMocks({
+        method: "PUT",
+        query: {
+          userId: "utenteTestCalendarioPOST",
+          nome: "utenteTestCalendarioPOST",
+          fusoOrario: {
+            GMTOffset: -5,
+            localita: "New York",
+          },
+          colore: "#123456",
+          partecipanti: ["utenteTestCalendarioPOST"],
+          principale: true,
+          impostazioniPredefiniteEventi: {
+            titolo: "",
+            descrizione: "",
+            durata: 30,
+            tempAnticNotifica: -30,
+            luogo: {
+              latitudine: 12.123456,
+              longitudine: 12.123456,
+            },
+            priorita: 6,
+            difficolta: 6,
+          },
+        },
+      });
+
+      await creaCalendario(req, res);
+
+      expect(res._getStatusCode()).toBe(400);
+      expect(JSON.parse(res._getData())).toEqual(
+        expect.objectContaining({
+          error: "Wrong format impostazioni predefinite",
         })
       );
     });
